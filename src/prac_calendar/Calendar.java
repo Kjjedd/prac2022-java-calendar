72da04d9df5f9d4102c4c0fdfe4376b5f108c394
Kjjedd <vocolate17@gmail.com>
2022-12-09 11:32:40 +0900

    Add weekday

--- src/prac_calendar/Calendar.java
+++ src/prac_calendar/Calendar.java
@@ -1,12 +1,10 @@
 package prac_calendar;
-
 /**
  * 월을 입력하면 해당월의 달력을 출력한다.
  * 1일은 일요일로 정해도 무방하다.
  * -1을 입력받기 전까지 반복 입력받는다.
  * 프롬프트를 출력한다.
  */
-
 public class Calendar {
     private static final int[] MAX_DAYS = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
     private static final int[] LEAP_MAX_DAYS = {31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
@@ -26,16 +24,35 @@ public class Calendar {
             return MAX_DAYS[month-1];
     }
 
-    public void printCalendar(int year, int month) {
+    public void printCalendar(int year, int month, int weekday) {
         System.out.printf("           <<%d년 %d월>>\n", year, month);
         System.out.println("  SUN  MON  TUE  WED  THU  FRI  SAT");
         System.out.println("  ---------------------------------");
-        int maxDays = getMaxDays(year, month);
-        for (int i = 1; i <= maxDays; i++) {
-            System.out.printf("%5d", i);
-            if(i%7==0)
-            System.out.println();
+
+        //print blank space
+        for (int i = 0; i < weekday; i++) {
+            System.out.print("     ");
         }
+
+        //print first line
+        int maxDays = getMaxDays(year, month);
+        int count = 7 - weekday;
+        int delim = (count < 7) ? count : 0;
+
+        for (int i = 1; i <= count; i++) {
+            System.out.printf("%5d", i);
+        }
+        System.out.println();
+
+        //print from second to last line
+        count++;
+        for (int i = count; i <= maxDays; i++) {
+            System.out.printf("%5d", i);
+            if (i % 7 == delim) {
+                System.out.println();
+            }
+        }
+
 
         System.out.println("\n  ---------------------------------");
     }
--- src/prac_calendar/Prompt.java
+++ src/prac_calendar/Prompt.java
@@ -5,34 +5,56 @@ import java.util.Scanner;
 public class Prompt {
     private final static String PROMPT_YEAR = "YEAR > ";
     private final static String PROMPT_MONTH = "MONTH > ";
-
-    public void runPrompt() {
-        Scanner scanner = new Scanner(System.in);
-        Calendar cal = new Calendar();
-        while (true) {
-            System.out.println("Enter the year");
-            System.out.print(PROMPT_YEAR);
-            int year = scanner.nextInt();
-            System.out.println("Enter the month");
-            System.out.print(PROMPT_MONTH);
-            int month = scanner.nextInt();
-            if (month == -1 || year == -1) {
-                System.out.println("Done");
-                break; //loop에서 탈출함
-            }
-            if (month > 12) {
-                System.out.println("Enter the month less than 12");
-                continue; //loop의 처음으로 돌아감
-            }
-            cal.printCalendar(year, month);
-
-        }
-    }
+    private final static String PROMPT_WEEKDAY = "WEEKDAY > ";
     public static void main(String[] args) {
         //셀 실행
         Prompt p = new Prompt();
         p.runPrompt();
-
     }
 
+    /**
+     * @param week: 요일명 (MON~SUN)
+     * @return 0~6 (0 = Sunday, 6 = Saturday)
+     */
+    public int parseDay(String week){
+        if(week.equals("SUN")) return 0;
+        else if(week.equals("MON")) return 1;
+        else if(week.equals("TUE")) return 2;
+        else if(week.equals("WED")) return 3;
+        else if(week.equals("THU")) return 4;
+        else if(week.equals("FRI")) return 5;
+        else if(week.equals("SAT")) return 6;
+        else return 0;
+
+    }
+    public void runPrompt() {
+        Scanner scanner = new Scanner(System.in);
+        Calendar cal = new Calendar();
+
+        while (true) {
+            System.out.println("Enter the year. (exit: -1)");
+            System.out.print(PROMPT_YEAR);
+            int year = scanner.nextInt();
+            if(year == -1){
+                System.out.println("EXIT");
+                break; //loop에서 탈출함
+            }
+
+            System.out.println("Enter the month.");
+            System.out.print(PROMPT_MONTH);
+            int month = scanner.nextInt();
+            if (month > 12 || month < 1) {
+                System.out.println("Invaild Input");
+                continue; //loop의 처음으로 돌아감
+            }
+
+            System.out.println("Enter the weekday. (Sun, Mon, Tue, Wed, Thu, Fri, Sat)");
+            System.out.print(PROMPT_WEEKDAY);
+            String str_weekday = scanner.next();
+            int weekday = parseDay(str_weekday);
+
+            cal.printCalendar(year, month, weekday);
+
+        }
+    }
 }
